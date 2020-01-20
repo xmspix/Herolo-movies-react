35469ef0f590f261513466b20620172a2eb00cb5
mark.stoler.86@gmail.com <mark.stoler.86@gmail.com>
2020-01-20 16:41:48 +0200

    Updates

---
+++ precache-manifest.298bdd7cbe991ed5f71bf9b06ab112c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9985fdf2ca839c2c6d533f6895b160f4",
+    "url": "/Herolo-movies-react/index.html"
+  },
+  {
+    "revision": "b5e6dc6ca257533b4ff9",
+    "url": "/Herolo-movies-react/static/css/main.ffa6d33c.chunk.css"
+  },
+  {
+    "revision": "beb2fa9ac46248c9efce",
+    "url": "/Herolo-movies-react/static/js/2.611c9961.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Herolo-movies-react/static/js/2.611c9961.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5e6dc6ca257533b4ff9",
+    "url": "/Herolo-movies-react/static/js/main.b59ab5dd.chunk.js"
+  },
+  {
+    "revision": "0381940c4036aff71354",
+    "url": "/Herolo-movies-react/static/js/runtime-main.b84c66bc.js"
+  }
+]);
